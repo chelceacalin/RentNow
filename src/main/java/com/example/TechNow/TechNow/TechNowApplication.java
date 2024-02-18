e5683391e0756887419e0e3f6ec9b19e7147ee07
Calin <chelcea.calin@yahoo.com>
2024-02-18 21:26:10 +0200

    project init

---
+++ src/main/java/com/example/TechNow/TechNow/TechNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechNow.TechNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNowApplication.class, args);
+	}
+
+}
