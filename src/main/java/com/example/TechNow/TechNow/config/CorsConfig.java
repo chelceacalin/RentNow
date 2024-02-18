f5037c5b280138e63258075925d2a20922c25439
Calin <chelcea.calin@yahoo.com>
2024-02-18 21:42:29 +0200

    added initial files

---
+++ src/main/java/com/example/TechNow/TechNow/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.TechNow.TechNow.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	@Value("${custom.frontend.app-url}")
+	private String frontendAppUrl;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(frontendAppUrl)
+				.allowedHeaders("*")
+				.allowedMethods("POST","PUT","GET","DELETE")
+				.allowCredentials(true);
+	}
+}
