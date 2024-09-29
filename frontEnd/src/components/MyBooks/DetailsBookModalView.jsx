d4fe4e184d5d69276c758933ace330c0e48e8b5d
Calin <chelcea.calin@yahoo.com>
2024-09-29 13:20:03 +0300

    Fix details window

--- frontEnd/src/components/MyBooks/DetailsBookModalView.jsx
+++ frontEnd/src/components/MyBooks/DetailsBookModalView.jsx
@@ -1,106 +1,28 @@
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  Autocomplete,
-  Button,
-  Dialog,
-  DialogContent,
-  TextField,
-} from "@mui/material";
+import { Autocomplete, Dialog, DialogContent, TextField } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import * as moreClasses from "react-dom/test-utils";
 import { showError, showSuccess } from "../../service/ToastService";
 
-function DetailsBookModalView({
-  isModalOpen,
-  defaultTitle,
-  defaultDirector,
-  isAvailable,
-  defaultCategory,
-  id,
-  triggerRefresh,
-  setTriggerRefresh,
-  photoUrl,
-  closeModal,
-}) {
-  const [selectedImage, setSelectedImage] = useState(photoUrl);
-  const [availableCategories, setAvailableCategories] = useState([]);
-  const [title, setTitle] = useState(defaultTitle);
-  const [director, setDirector] = useState(defaultDirector);
-  const [description, setDescription] = useState("");
-  const [category, setCategory] = useState(defaultCategory);
-  const [imagePreviewUrl, setImagePreviewUrl] = useState(photoUrl);
-
+function DetailsBookModalView({ isModalOpen, book, onRefresh, closeModal }) {
   const MAX_FILE_SIZE = 2048 * 2048;
-  useEffect(() => {
-    axios.get(`/books/${id}`).then((data) => {
-      if (data.data.description.length > 0) {
-        setDescription(data.data.description);
-      }
-    });
-  }, []);
 
-  const validationChecks = [
-    {
-      condition: !title || title.length < 2,
-      message: "Title should have at least 2 characters!",
-    },
-    {
-      condition: !director || director.length < 2,
-      message: "Director should have at least 2 characters!",
-    },
-    {
-      condition: !description || description.length < 2,
-      message: "Description should have at least 2 characters!",
-    },
-  ];
-
-  const handleImageBrowse = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      if (file.size > MAX_FILE_SIZE) {
-        showError("Image size should be no more than 2 MB!");
-        setSelectedImage(null);
-        setImagePreviewUrl(null);
-      } else if (
-        !["image/jpeg", "image/jpg", "image/png"].includes(file.type)
-      ) {
-        showError("Image format should be jpg, jpeg, or png!");
-        setSelectedImage(null);
-        setImagePreviewUrl(null);
-      } else {
-        setSelectedImage(file);
-        setImagePreviewUrl(URL.createObjectURL(file));
-      }
-    }
-  };
-
-  const handleImageDrop = (event) => {
-    event.preventDefault();
-    const file = event.dataTransfer.files[0];
-    if (file) {
-      if (file.size > MAX_FILE_SIZE) {
-        showError("Image size should be no more than 2 MB!");
-        setSelectedImage(null);
-        setImagePreviewUrl(null);
-      } else if (
-        !["image/jpeg", "image/jpg", "image/png"].includes(file.type)
-      ) {
-        showError("Image format should be jpg, jpeg, or png!");
-        setSelectedImage(null);
-        setImagePreviewUrl(null);
-      } else {
-        setSelectedImage(file);
-        setImagePreviewUrl(URL.createObjectURL(file));
-      }
-    }
-  };
+  const [availableCategories, setAvailableCategories] = useState([]);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(book.photoUrl || "");
+  const [formData, setFormData] = useState({
+    title: book.title || "",
+    director: book.director || "",
+    description: book.description || "",
+    category: book.category || "",
+    selectedImage: book.photoUrl || "",
+    isAvailable: book.isAvailable,
+  });
+  const [formErrors, setFormErrors] = useState({});
 
   useEffect(() => {
-    let url = `/category`;
     axios
-      .get(url)
+      .get(`/category`)
       .then((response) => {
         setAvailableCategories(response.data.content);
       })
@@ -109,91 +31,106 @@ function DetailsBookModalView({
       });
   }, []);
 
-  const validRequest = () => {
-    for (const check of validationChecks) {
-      if (check.condition) {
-        showError(check.message);
-        return false;
-      }
-    }
-    return true;
+  const handleInputChange = (field) => (event, value) => {
+    const newValue = value !== undefined ? value : event.target.value;
+    setFormData((prev) => ({
+      ...prev,
+      [field]: newValue,
+    }));
+    setFormErrors((prev) => ({
+      ...prev,
+      [field]: null,
+    }));
   };
 
-  const validFields = () => {
-    let valid = true;
-    if (title.charAt(0) !== title.charAt(0).toUpperCase()) {
-      showError("Title should start with an uppercase letter!");
-      valid = false;
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (
+      file &&
+      file.size <= MAX_FILE_SIZE &&
+      ["image/jpeg", "image/png"].includes(file.type)
+    ) {
+      setFormData((prev) => ({ ...prev, selectedImage: file }));
+      setFormErrors((prev) => ({ ...prev, selectedImage: null }));
+      setImagePreviewUrl(URL.createObjectURL(file));
+    } else {
+      setFormErrors((prev) => ({
+        ...prev,
+        selectedImage: "Invalid image file",
+      }));
+      setImagePreviewUrl(null);
     }
+  };
 
-    if (director.charAt(0) !== director.charAt(0).toUpperCase()) {
-      showError("Director should start with an uppercase letter!");
-      valid = false;
+  const validateForm = () => {
+    const { title, director, category, description, selectedImage } = formData;
+    const errors = {};
+
+    if (
+      !title ||
+      title.length < 2 ||
+      title.charAt(0) !== title.charAt(0).toUpperCase()
+    ) {
+      errors.title =
+        "Title should have at least 2 characters and start with an uppercase letter";
     }
-    return valid;
+    if (
+      !director ||
+      director.length < 2 ||
+      director.charAt(0) !== director.charAt(0).toUpperCase()
+    ) {
+      errors.director =
+        "Director should have at least 2 characters and start with an uppercase letter";
+    }
+    if (!category) errors.category = "Please select a category";
+    if (!description) errors.description = "Description should not be empty";
+    if (!selectedImage) errors.selectedImage = "Please select an image";
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
   };
 
   const handleSave = () => {
-    if (validRequest()) {
-      if (!selectedImage) {
-        showError("Image should not be empty!");
-        return;
-      }
-
-      if (validFields()) {
-        if (!category) {
-          showError("Category " + category + " does not exist ");
-        } else {
-          let finalCategory = category;
-
-          const formData = new FormData();
-          let bookDTO = {
-            title: title,
-            director: director,
-            description: description,
-            category: finalCategory,
-          };
-
-          formData.append(
-            "bookDTO",
-            new Blob([JSON.stringify(bookDTO)], { type: "application/json" })
-          );
-
-          console.log("selected image" + selectedImage);
-          if (selectedImage) {
-            formData.append("imageFile", selectedImage);
-          }
-
-          axios
-            .post(`/books/${id}`, formData, {
-              headers: {
-                "Content-Type": "multipart/form-data",
-              },
-            })
-            .then(() => {
-              setTriggerRefresh(!triggerRefresh);
-              showSuccess("Book edited successfully!");
-              resetForm();
-              closeModal();
-            })
-            .catch((err) => {
-              showError("Error editing book: " + err.message);
-            });
-        }
-      }
+    if (!validateForm()) {
+      showError("Please fix the errors in the form");
+      return;
     }
-  };
 
-  const resetForm = () => {
-    setTitle("");
-    setDirector("");
-    setDescription("");
-    setCategory("");
-    setSelectedImage(null);
+    const formDataToSubmit = new FormData();
+    const bookDTO = {
+      title: formData.title,
+      director: formData.director,
+      description: formData.description,
+      category: formData.category,
+    };
+
+    formDataToSubmit.append(
+      "bookDTO",
+      new Blob([JSON.stringify(bookDTO)], { type: "application/json" })
+    );
+
+    if (formData.selectedImage) {
+      formDataToSubmit.append("imageFile", formData.selectedImage);
+    }
+
+    axios
+      .post(`/books/${book.id}`, formDataToSubmit, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then(() => {
+        showSuccess("Book edited successfully!");
+        onRefresh();
+        closeModal();
+      })
+      .catch((err) => {
+        showError("Error editing book: " + err.message);
+      });
   };
 
   return (
-    <Dialog maxWidth={"md"} open={isModalOpen} onClose={closeModal}>
+    <Dialog open={isModalOpen} onClose={closeModal} maxWidth="sm" fullWidth>
       <div className="modal-content wider-modal">
         <div className="header-container">
           <FontAwesomeIcon
@@ -202,164 +139,112 @@ function DetailsBookModalView({
             size="xl"
             onClick={closeModal}
           />
-          <div className="w-full">
-            <h2 className="header-title ml-6 mt-10">Edit book</h2>
-          </div>
+          <h2 className="ml-8 mt-6 text-2xl">Edit Book</h2>
         </div>
         <DialogContent className="modal-body ml-2 mr-2">
-          <div>
-            <TextField
-              label="Title"
-              required
-              disabled={!isAvailable}
-              sx={{
-                width: { md: 835 },
-              }}
-              defaultValue={title}
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
-              InputProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
-              InputLabelProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
+          <div className="flex flex-col space-y-4 mb-6">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              id="imageUpload"
+              disabled={!formData.isAvailable}
+              className="hidden"
             />
-          </div>
-          <div className="mt-6">
-            <TextField
-              required
-              label="Director"
-              sx={{
-                width: { md: 835 },
-              }}
-              defaultValue={director}
-              onChange={(e) => {
-                setDirector(e.target.value);
-              }}
-              InputProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
-              InputLabelProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
-              disabled={!isAvailable}
-            />
-          </div>
-          <div className="mt-6">
-            <Autocomplete
-              onChange={(e, value) => setCategory(value)}
-              value={category}
-              sx={{
-                width: { md: 835 },
-              }}
-              options={availableCategories.map((c) => c.name)}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  InputLabelProps={{
-                    style: { fontFamily: "Sanchez" },
-                  }}
-                  InputProps={{
-                    ...params.InputProps,
-                    ...moreClasses.input,
-                    style: { fontFamily: "Sanchez" },
-                  }}
-                  sx={{ fontFamily: "Sanchez" }}
-                  label="Category"
-                />
-              )}
-              disabled={!isAvailable}
-            />
-          </div>
-          <div className="mt-6">
-            <TextField
-              required
-              placeholder=" Write a description for the book..."
-              label="Description"
-              multiline={true}
-              className="textarea-field w-full border-2 p-2"
-              value={description}
-              rows={4}
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-              InputProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
-              InputLabelProps={{
-                style: { fontFamily: "Sanchez" },
-              }}
-              disabled={!isAvailable}
-            />
-          </div>
+            <label
+              htmlFor="imageUpload"
+              className={`rent-button -sm ${
+                formData.isAvailable ? "" : "disabled"
+              }`}
+            >
+              Upload Image
+            </label>
 
-          <div className="field-group image-upload-field">
-            <div className="mt-6">
-              <h2 className="text-xl font-bold mb-4">Image Upload</h2>
-              <div
-                className="border-2 border-gray-400 p-4 rounded-lg mb-4"
-                onDrop={() => {
-                  if (!isAvailable) {
-                    showError("You cannot drop images because book is rented!");
-                  } else {
-                    handleImageDrop();
-                  }
-                }}
-                onDragOver={(event) => {
-                  event.preventDefault();
-                }}
-              >
-                {imagePreviewUrl ? (
+            {formErrors.selectedImage && (
+              <p className="bg-main-color text-sm">
+                {formErrors.selectedImage}
+              </p>
+            )}
+
+            {imagePreviewUrl && (
+              <div className="mt-4">
+                <h3 className="text-lg font-semibold text-gray-700 mb-2">
+                  Image Preview
+                </h3>
+                <div className="border rounded-lg overflow-hidden w-full h-52">
                   <img
                     src={imagePreviewUrl}
                     alt="Selected"
-                    className="w-40 h-40 object-cover rounded-lg"
-                    disabled={!isAvailable}
+                    className="object-cover w-full h-full"
                   />
-                ) : (
-                  <div className="text-gray-500">
-                    Drop or Browse to select an image
-                  </div>
-                )}
+                </div>
               </div>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageBrowse}
-                className="mb-4 w-full"
-                disabled={!isAvailable}
-              />
-            </div>
-          </div>
-          <div className="modal-footer mt-4 flex gap-x-2">
-            <div className="flex-1">
-              <Button
-                type="button"
-                onClick={handleSave}
-                className="w-full darkButton"
-                variant="contained"
-                disabled={!isAvailable}
-              >
-                Save
-              </Button>
-            </div>
+            )}
 
-            <div className="flex-1">
-              <Button
-                type="button"
-                onClick={() => {
-                  console.log("Click");
-                  closeModal();
-                  console.log("open " + isModalOpen);
-                }}
-                className="outlined-button w-full"
-                variant="outlined"
-              >
-                Close
-              </Button>
-            </div>
+            <TextField
+              fullWidth
+              label="Title"
+              value={formData.title}
+              onChange={handleInputChange("title")}
+              variant="outlined"
+              error={!!formErrors.title}
+              helperText={formErrors.title}
+              disabled={!formData.isAvailable}
+            />
+
+            <TextField
+              fullWidth
+              label="Director"
+              value={formData.director}
+              onChange={handleInputChange("director")}
+              variant="outlined"
+              error={!!formErrors.director}
+              helperText={formErrors.director}
+              disabled={!formData.isAvailable}
+            />
+
+            <Autocomplete
+              onChange={handleInputChange("category")}
+              value={formData.category}
+              options={availableCategories.map((c) => c.name)}
+              renderInput={(params) => (
+                <TextField {...params} label="Category" />
+              )}
+              error={!!formErrors.category}
+              helperText={formErrors.category}
+              disabled={!formData.isAvailable}
+            />
+
+            <TextField
+              fullWidth
+              label="Description"
+              value={formData.description}
+              onChange={handleInputChange("description")}
+              variant="outlined"
+              multiline
+              rows={4}
+              error={!!formErrors.description}
+              helperText={formErrors.description}
+              disabled={!formData.isAvailable}
+            />
+          </div>
+
+          <div className="mt-6 flex justify-end space-x-4">
+            <button
+              onClick={handleSave}
+              className="details-button"
+              disabled={!formData.isAvailable}
+            >
+              Save
+            </button>
+            <button
+              onClick={(e) => {
+                closeModal();
+              }}
+              className="details-button details-button-red"
+            >
+              Cancel
+            </button>
           </div>
         </DialogContent>
       </div>
